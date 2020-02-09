ef6236c3c6de12baa2712b1f52dc160ddf453bd5
Mariusz Jabłoński <amidek321@gmail.com>
2020-02-09 10:41:02 +0100

    poprawa

--- app/script.js
+++ app/script.js
@@ -1,75 +1,55 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-formatTime = () => {
+const state = {
+  status: {
+    default: 'off',
+    work: 'work',
+    rest: 'rest',
+  },
+  time: 1200,
+  timer: null,
+}
+
+const formatTime = () => {
   format = () => {
-  let minutes = Math.floor(this.time / 60);
-  let seconds = this.time - (minutes * 60);
-  if (minutes < 10) {minutes = "0"+minutes;}
-  if (seconds < 10) {seconds = "0"+seconds;}
-  return minutes+':'+seconds;
+    let minutes = Math.floor(state.time / 60);
+    let seconds = state.time - (minutes * 60);
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return minutes+':'+seconds;
   }
   this.setState ({
-    time: format(this.timer),
+    time: format(state.timer),
   });
 };
 
-step = () => {
-  for (i=time; i=0; i--) {
-    if (state.status === 'work') {
-      state.status = 'rest';
-      time = 20;
-      playBell();
-    } else {
-      state.status = 'work';
-      time = 1200;
-      playBell();
-    }
-  }
-};
+const step = () => {};
 
-startTimer = () => {
-time = 1200;
+const startTimer = () => {
+  time = state.time;
   this.setState({
     timer: setInterval(this.step, 1000),
   });
-state.status = 'work';
-};
-
-stopTimer = () => {
-  window.clearInterval(timer);
-  state.time = 0;
-  state.status = 'off';
-};
-
-closeApp = () => {
-  window.close();
-};
-
-playBell = () => {
-  const bell = new Audio('./sounds/bell.wav');
-  bell.play();
+  state.status === 'work';
 };
 
 class App extends React.Component {
 
-render() {
-
-  const { status } = this.state;
-
-  return (
-    <div>
-      <h1>Protect your eyes</h1>
-      {(status === 'off') && <AppDescription />}
-      {(status === 'work') && <img src="./images/work.png" />}
-      {(status === 'rest') && <img src="./images/rest.png" />}
-      {(status !== 'off') && <div className="timer">{this.formatTime}</div>}
-      {(status === 'off') && <button className="btn" onClick={tartTimer}>Start</button>}
-      {(status !== 'off') && <button className="btn" onCLick={stopTimer}>Stop</button>}
-      <button className="btn btn-close" onClick={closeApp}>X</button>
-    </div>
-  )
- };
-}
+  render() {
+    return (
+      <div>
+        <h1>Protect your eyes</h1>
+        {(status === 'off') && <AppDescription />}
+        {(status === 'work') && <img src="./images/work.png" />}
+        {(status === 'rest') && <img src="./images/rest.png" />}
+        {(status !== 'off') && <div className="timer">{state.timer}</div>}
+        {(status === 'off') && <button className="btn" onClick={startTimer}>Start</button>}
+        {(status !== 'off') && <button className="btn" onClick={this.stopTimer}>Stop</button>}
+        <button className="btn btn-close">X</button>
+      </div>
+    )
+  }
+};
 
 render(<App />, document.querySelector('#app'));
