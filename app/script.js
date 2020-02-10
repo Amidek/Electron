c4ebfb0badee99ee7e06d44f1717ff7727eb98ae
Mariusz Jabłoński <amidek321@gmail.com>
2020-02-10 07:26:53 +0100

    uzupełnienie aplikacji w działający kod

--- app/script.js
+++ app/script.js
@@ -1,52 +1,85 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const state = {
-  status: {
-    default: 'off',
-    work: 'work',
-    rest: 'rest',
-  },
-  time: 1200,
-  timer: null,
-}
+class App extends React.Component {
 
-const formatTime = () => {
-  format = () => {
-    let minutes = Math.floor(state.time / 60);
-    let seconds = state.time - (minutes * 60);
+  constructor() {
+    super()
+    this.state = {
+      status: 'off',
+      time: 0,
+      timer: null
+    };
+  }
+
+  step = () => {
+    this.setState({
+      time: this.state.time - 1,
+    });
+    if (this.state.time === 0) {
+      if (this.state.status === 'work'){
+        this.setState({
+          status: 'rest',
+          time: 20,
+        })
+        this.playBell()
+      }
+      else if (this.state.status === 'rest'){
+        this.setState({
+          status: 'work',
+          time: 1200,
+        })
+        this.playBell()
+      }
+    }
+  };
+
+  startTimer = () => {
+    this.setState({
+      timer: setInterval(this.step, 1000),
+      time: 1200,
+      status: 'work',
+    });
+  };
+
+  stopTimer = () => {
+    this.setState({
+      status: 'off',
+      time: 0,
+    })
+    clearInterval(this.state.timer);
+  }
+
+  formatTime = (time) => {
+    let minutes = Math.floor(time/60)
+    let seconds = time-(minutes*60)
     if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
     return minutes+':'+seconds;
+  };
+
+  closeApp = () => window.close();
+
+  playBell = () => {
+    var audioElement = new Audio('./sounds/bell.wav');
+    audioElement.play();
   }
-  this.setState ({
-    time: format(state.timer),
-  });
-};
-
-const step = () => {};
-
-const startTimer = () => {
-  time = state.time;
-  this.setState({
-    timer: setInterval(this.step, 1000),
-  });
-  state.status === 'work';
-};
-
-class App extends React.Component {
 
   render() {
+
+    const { status, time} = this.state;
+
     return (
       <div>
         <h1>Protect your eyes</h1>
-        {(status === 'off') && <AppDescription />}
-        {(status === 'work') && <img src="./images/work.png" />}
-        {(status === 'rest') && <img src="./images/rest.png" />}
-        {(status !== 'off') && <div className="timer">{state.timer}</div>}
-        {(status === 'off') && <button className="btn" onClick={startTimer}>Start</button>}
-        {(status !== 'off') && <button className="btn" onClick={this.stopTimer}>Stop</button>}
-        <button className="btn btn-close">X</button>
+        {(status === 'off') && <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>}
+        {(status === 'off') && <p>This app will help you track your time and inform you when it's time to rest.</p>}
+        {(status === 'work') && <img src="./images/Work.png" />}
+        {(status === 'rest') && <img src="./images/Rest.png" />}
+        {(status !== 'off') && <div className="timer">{this.formatTime(time)}</div>}
+        {(status === 'off') && <button className="btn" onClick={(e) => this.startTimer(e)}>Start</button>}
+        {(status !== 'off') && <button className="btn" onClick={(e) => this.stopTimer(e)}>Stop</button>}
+        <button className="btn btn-close" onClick={(e) => this.closeApp(e)}>X</button>
       </div>
     )
   }
